981baccdcd445de1fa5127ec085c7678d3ea94eb
apcagcatap <agcatap@student.apc.edu.ph>
2025-12-15 11:13:09 +0000

    Register Users App Models

--- backend/maker_backend/users/admin.py
+++ backend/maker_backend/users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Profile
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ['username', 'email', 'user_type', 'is_staff', 'date_joined']
+    list_filter = ['user_type', 'is_staff', 'is_active']
+    search_fields = ['username', 'email']
+    
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('user_type', 'supabase_id')}),
+    )
+    
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('user_type',)}),
+    )
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created_at']
+    search_fields = ['user__username', 'user__email']
+    raw_id_fields = ['user']
