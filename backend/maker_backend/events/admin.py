9b1eb2903d5da6c6c098eacbeca95d2e38556347
apcagcatap <agcatap@student.apc.edu.ph>
2025-12-15 11:13:32 +0000

    Register Events App Models

--- backend/maker_backend/events/admin.py
+++ backend/maker_backend/events/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'location', 'start_date', 'end_date', 'is_active', 'created_by']
+    list_filter = ['is_active', 'start_date']
+    search_fields = ['title', 'description', 'location']
+    date_hierarchy = 'start_date'
+    raw_id_fields = ['created_by']
+    
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('title', 'description', 'location')
+        }),
+        ('Schedule', {
+            'fields': ('start_date', 'end_date', 'is_active')
+        }),
+        ('Management', {
+            'fields': ('created_by',)
+        }),
+    )
